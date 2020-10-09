4f1afa2f570bada1fb7b3bdb644e9f7dd178122a
guapc <apcgus@gmail.com>
2020-10-09 09:38:23 -0500

    First version

---
+++ ColorsV1.js
@@ -0,0 +1,11 @@
+import Colors from './Colors'
+
+const colorsV1 = {
+    ...Colors,
+    primary: '#FAE345',
+    accent: '#FAE345',
+    background: '#4A4F54',
+    card: '#4A4F54',
+};
+
+export default colorsV1;
---
+++ Metrics.js
@@ -0,0 +1,15 @@
+// Used via Metrics.baseMargin
+const metrics = {
+	doubleBaseMargin: 32,
+	baseMargin: 16,
+	smallMargin: 8,
+	tinyMargin: 4,
+	buttonRadius: 10,
+	icons: {
+		small: 16,
+		regular: 24,
+		large: 48,
+	}
+};
+
+export default metrics;
