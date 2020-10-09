5bcb1df7f82e83329804181a291a3da455b14b8a
guapc <apcgus@gmail.com>
2020-10-09 13:24:32 -0500

    v 1.0.1

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import Colors from './Colors';
+import ColorsV1 from './ColorsV1';
 import Fonts from './Fonts';
 import Metrics from './Metrics';
 import ApplicationStyles from './ApplicationStyles';
 
-export { Colors, Fonts, Metrics, ApplicationStyles };
+module.exports = { ColorsV1, Colors, Fonts, Metrics, ApplicationStyles };
